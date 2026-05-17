18592914a64a127214b6cd8c33cae3a51960ee69
dlvga <dlvga@mail.com>
2026-05-17 16:56:39 +0200

    fix(login,profile): fix background color

--- front/src/components/Layout/Layout.jsx
+++ front/src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import styles from './Layout.module.scss'
-import {Outlet} from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
-function Layout({ children }) {
+function Layout() {
+  const { pathname } = useLocation()
+
   return (
     <>
       <Navbar />
-      <main className={styles.main}>
+      <main className={`${styles.main} ${['/login', '/profile'].includes(pathname) ? styles.mainDark : ''}`}>
         <Outlet />
       </main>
       <Footer />
